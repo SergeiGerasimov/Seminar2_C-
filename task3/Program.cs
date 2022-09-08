b42e1eb8c803568cd3e684b50da5307139d1382b
SergeiGerasimov <110515673+SergeiGerasimov@users.noreply.github.com>
2022-09-08 09:53:35 +0700

    Кратно 7 и 23

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите прогамму, которая принимает на вход число
+// и приверяет, кратно ли оно одновременно 7 и 23
+// 14 - нет 
+// 46 - нет 
+// 161 - да 
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Multip(int num, int a, int b)
+{
+    return num % a == 0 && num % b == 0; // Возвращает True, если условие работает
+}
+if (Multip(number, 7, 23)) Console.WriteLine($"{number} -> Да");
+else Console.WriteLine($"{number} -> Нет");
